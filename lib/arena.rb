2d506b6224267075ba304f143ae8c1defd68b140
mwb86 <masonwbrown@gmail.com>
2016-12-09 15:17:25 -0500

    finished

--- lib/arena.rb
+++ lib/arena.rb
@@ -12,11 +12,31 @@ class Arena
     return @gladiators
   end
 
-  def add_gladiator
+  def add_gladiator(new_gladiator)
+    if @gladiators.length < 2
+    @gladiators.push(new_gladiator)
+    end
   end
 
   def fight
+  
 
+    if @gladiators.count == 2
+      if @gladiators.first.weapon == @gladiators.last.weapon
+        @gladiators.pop(2)
+      elsif @gladiators.first.weapon == "Trident" && @gladiators.last.weapon == "Club"
+        @gladiators.delete_at(0)
+      elsif @gladiators.first.weapon == "Trident" && @gladiators.last.weapon == "Spear"
+        @gladiators.delete_at(1)
+      elsif @gladiators.first.weapon == "Spear" && @gladiators.last.weapon == "Trident"
+        @gladiators.delete_at(0)
+      elsif @gladiators.first.weapon == "Spear" && @gladiators.last.weapon == "Club"
+        @gladiators.delete_at(1)
+      elsif @gladiators.first.weapon == "Club" && @gladiators.last.weapon == "Trident"
+        @gladiators.delete_at(1)
+      elsif @gladiators.first.weapon == "Club" && @gladiators.last.weapon == "Spear"
+        @gladiators.delete_at(0)
+    end
   end
-
+end
 end

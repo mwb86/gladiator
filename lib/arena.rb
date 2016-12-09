9e517a348e13ecdc643b82088a9be8718c763395
mwb86 <masonwbrown@gmail.com>
2016-12-09 14:03:39 -0500

    arena starts with 0 gladiators

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,22 @@
 class Arena
+  def initialize (name)
+    @name = name.capitalize
+    @gladiators = []
+  end
+
+  def name
+    return @name
+  end
+
+  def gladiators
+    return @gladiators
+  end
+
+  def add_gladiator
+  end
+
+  def fight
+
+  end
+
 end

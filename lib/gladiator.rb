94ad09f4e16f14c0d67d2212d5fbf74b95ecf6e3
mwb86 <masonwbrown@gmail.com>
2016-12-09 13:43:22 -0500

    passed gladiator

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,15 @@
 class Gladiator
+  def initialize (name, weapon)
+    @name = name
+    @weapon = weapon
+  end
+
+  def name
+    return @name
+  end
+
+  def weapon
+    return @weapon
+  end
+
 end
